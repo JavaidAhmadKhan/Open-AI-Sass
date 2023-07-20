230d8722c43b87ba760d27635cf06f140a96ea18
JavaidAhmadKhan <relaxjavaid@gmail.com>
2023-07-20 11:45:33 +0530

    finihed dashboard

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -2,7 +2,8 @@
 
 import { Card } from "@/components/ui/card"
 import { cn } from "@/lib/utils"
-import { ArrowRight, MessageSquare } from "lucide-react"
+import { ArrowRight, CodeIcon, ImageIcon, MessageSquare, Music, VideoIcon } from "lucide-react"
+import { useRouter } from "next/navigation"
 
 const tools = [
     {
@@ -11,10 +12,41 @@ const tools = [
         color: 'text-violet-500',
         bgColor: 'bg-violet-500/10',
         href: '/conversation'
-    }
+    },
+    {
+        label: 'Music Generation',
+        icon: Music,
+        color: 'text-pink-700',
+        bgColor: 'bg-pink-700/10',
+        href: '/image'
+    },
+    {
+        label: 'Video Generation',
+        icon: VideoIcon,
+        color: 'text-orange-700',
+        bgColor: 'bg-orange-700/10',
+        href: '/video'
+    },
+    {
+        label: 'Code Generation',
+        icon: CodeIcon,
+        color: 'text-green-700',
+        bgColor: 'bg-green-700/10',
+        href: '/code'
+    },
+
+
+    {
+        label: 'Image Generation',
+        icon: ImageIcon,
+        color: 'text-violet-500',
+        bgColor: 'bg-violet-500/10',
+        href: '/conversation'
+    },
 ]
 
 const Dashboard = () => {
+    const router = useRouter()
     return (
         <div>
             <div className="mb-8 space-y-4">
@@ -24,6 +56,7 @@ const Dashboard = () => {
             <div className="px-4 md:px-20 lg:px-32 space-y-4">
                 {tools.map((tool) => (
                     <Card
+                        onClick={() => router.push(tool.href)}
                         key={tool.href}
                         className="p-4 border-black/5 flex items-center justify-between hover:shadow-md transition cursor-pointer"
                     >
