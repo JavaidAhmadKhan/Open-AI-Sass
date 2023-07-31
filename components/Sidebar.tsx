b44f8576493b3ca60afccdff64a5f6e0d7e1885b
JavaidAhmadKhan <relaxjavaid@gmail.com>
2023-07-31 11:54:30 +0530

    added Free counter component sidebar and functionality

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -6,6 +6,7 @@ import Link from "next/link"
 import { cn } from "@/lib/utils"
 import { CodeIcon, ImageIcon, LayoutDashboard, MessageSquare, MusicIcon, Settings, VideoIcon } from "lucide-react"
 import { usePathname } from 'next/navigation'
+import FreeCounter from "./FreeCounter"
 
 const montserrat = Montserrat({ weight: '600', subsets: ['latin'] })
 
@@ -53,7 +54,13 @@ const routes = [
     },
 ]
 
-const Sidebar = () => {
+interface SidebarProps {
+    apiLimitCount: number
+}
+
+const Sidebar = ({
+    apiLimitCount = 0
+}: SidebarProps) => {
     const pathname = usePathname();
     return (
         <div className="space-y-4 py-4 flex-col h-full bg-[#111827] text-white">
@@ -80,6 +87,9 @@ const Sidebar = () => {
                 </div>
 
             </div>
+            <FreeCounter
+                apiLimitCount={apiLimitCount}
+            />
         </div>
     )
 }
